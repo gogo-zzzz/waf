056aab5276ddbb65d9404f7623fa9af635adfd98
stormtrooper <stromtrooper@no.where>
2019-06-22 15:25:28 +0700

    fix log

--- log.lua
+++ log.lua
@@ -1,13 +1,13 @@
 function log_main()
     if config_log_cc_check == "on" then
-        -- log_record('CC_Attack',ngx.var.request_uri,"-","-",STATUS)
+        local STATUS = ngx.status
+        log_record('log_main()',ngx.var.request_uri,"-","-",STATUS)
         -- if STATUS == ngx.HTTP_NOT_FOUND || STATUS == ngx.HTTP_BAD_REQUEST || STATUS == ngx.HTTP_UNAUTHORIZED || STATUS == ngx.HTTP_NOT_ALLOWED then
         if (STATUS >= ngx.HTTP_NOT_FOUND and STATUS <
             ngx.HTTP_INTERNAL_SERVER_ERROR) then
             local ATTACK_URI = ngx.var.uri
             local CC_TOKEN = get_client_ip() .. ATTACK_URI
             local limit = ngx.shared.limit
-            local STATUS = ngx.status
             CCcount = tonumber(string.match(config_cc_rate, '(.*)/'))
             CCseconds = tonumber(string.match(config_cc_rate, '/(.*)'))
             local req, _ = limit:get(CC_TOKEN)
